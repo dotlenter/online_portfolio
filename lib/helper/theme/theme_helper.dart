cdf499fc68cdb4b5a20a251f704d3598031f8cdf
JM Lorenzo <lorenzojohnmar321@gmail.com>
2022-08-03 15:14:54 +0800

    Coding review done

--- lib/helper/theme/theme_helper.dart
+++ lib/helper/theme/theme_helper.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:injectable/injectable.dart';
+import 'package:injectable/injectable.dart'; // Remove this file
 
 @injectable
 class ThemeHelper {
