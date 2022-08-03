30a4682cf5efcc99649b4b376af44133f5695117
JM Lorenzo <lorenzojohnmar321@gmail.com>
2022-08-03 15:07:18 +0800

    Coding review done

--- lib/modules/home/modules/skill/widget/skill_icon_widget.dart
+++ lib/modules/home/modules/skill/widget/skill_icon_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:online_portfolio/modules/home/data/models/skill_icon.dart';
-import 'package:online_portfolio/modules/home/domain/image_getter.dart';
+import 'package:online_portfolio/modules/home/domain/image_getter.dart'; // <-- Fix imports
 
 class SkillIconWidget extends StatelessWidget {
   final SkillIcon skill;
